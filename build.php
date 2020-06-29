9b79a1e0be6ff555b06c01e870b55c87f6834b75
LexCTR <67603614+LexCTR@users.noreply.github.com>
2020-06-29 16:27:01 -0300

    Create build.php

---
+++ build.php
@@ -0,0 +1,222 @@
+<?php
+
+// github info
+
+$username = 'superlist';
+$repository = 'superlist';
+$branch = 'master';
+
+// export 1 - export file, export 0 - display
+$export = '1';
+$exportname = './output/superlist.txt';
+
+// local (fast), remote (slow)
+$source = 'local';
+
+$filtername = 'superlist';
+$home = 'https://github.com/LexCTR/adblock_filter_lists/';
+
+// default file
+$file1 = 'http://mirror1.malwaredomains.com/files/immortal_domains.txt';
+$file2 = 'https://adaway.org/hosts.txt';
+$file3 = 'https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt';
+$file4 = 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt';
+$file5 = 'https://easylist-downloads.adblockplus.org/adwarefilters.txt';
+$file6 = 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt';
+$file7 = 'https://filters.adtidy.org/extension/chromium/filters/10.txt';
+$file8 = 'https://filters.adtidy.org/extension/chromium/filters/15.txt';
+$file9 = 'https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoFilters/NanoBase.txt';
+$file10 = 'https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoMirror/NanoDefender.txt';
+$file11 = 'https://gitcdn.xyz/repo/NanoAdblockerLab/NanoContrib/master/dist/placeholder-buster.txt';
+$file12 = 'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt';
+$file13 = 'https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt';
+$file14 = 'https://openphish.com/feed.txt';
+$file15 = 'https://phishing.army/download/phishing_army_blocklist_extended.txt';
+$file16 = 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts';
+$file17 = 'https://raw.githubusercontent.com/brave/adblock-lists/master/brave-unbreak.txt';
+$file18 = 'https://raw.githubusercontent.com/easylistbrasil/easylistbrasil/filtro/easylistbrasil.txt';
+$file19 = 'https://raw.githubusercontent.com/IDKwhattoputhere/uBlock-Filters-Plus/master/uBlock-Filters-Plus.txt';
+$file20 = 'https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset';
+$file21 = 'https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level2.netset';
+$file22 = 'https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt';
+$file23 = 'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt';
+$file24 = 'https://v.firebog.net/hosts/Prigent-Ads.txt';
+$file25 = 'https://v.firebog.net/hosts/Prigent-Malware.txt';
+$file26 = 'https://v.firebog.net/hosts/Shalla-mal.txt';
+$file27 = 'https://www.i-dont-care-about-cookies.eu/abp/';
+$file28 = 'https://www.malwaredomainlist.com/hostslist/ip.txt';
+$list1 = 'http://mirror1.malwaredomains.com/files/immortal_domains.txt';
+$list2 = 'https://adaway.org/hosts.txt';
+$list3 = 'https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt';
+$list4 = 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt';
+$list5 = 'https://easylist-downloads.adblockplus.org/adwarefilters.txt';
+$list6 = 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt';
+$list7 = 'https://filters.adtidy.org/extension/chromium/filters/10.txt';
+$list8 = 'https://filters.adtidy.org/extension/chromium/filters/15.txt';
+$list9 = 'https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoFilters/NanoBase.txt';
+$list10 = 'https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoMirror/NanoDefender.txt';
+$list11 = 'https://gitcdn.xyz/repo/NanoAdblockerLab/NanoContrib/master/dist/placeholder-buster.txt';
+$list12 = 'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt';
+$list13 = 'https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt';
+$list14 = 'https://openphish.com/feed.txt';
+$list15 = 'https://phishing.army/download/phishing_army_blocklist_extended.txt';
+$list16 = 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts';
+$list17 = 'https://raw.githubusercontent.com/brave/adblock-lists/master/brave-unbreak.txt';
+$list18 = 'https://raw.githubusercontent.com/easylistbrasil/easylistbrasil/filtro/easylistbrasil.txt';
+$list19 = 'https://raw.githubusercontent.com/IDKwhattoputhere/uBlock-Filters-Plus/master/uBlock-Filters-Plus.txt';
+$list20 = 'https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset';
+$list21 = 'https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level2.netset';
+$list22 = 'https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt';
+$list23 = 'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt';
+$list24 = 'https://v.firebog.net/hosts/Prigent-Ads.txt';
+$list25 = 'https://v.firebog.net/hosts/Prigent-Malware.txt';
+$list26 = 'https://v.firebog.net/hosts/Shalla-mal.txt';
+$list27 = 'https://www.i-dont-care-about-cookies.eu/abp/';
+$list28 = 'https://www.malwaredomainlist.com/hostslist/ip.txt';
+
+Error_reporting (0);
+
+// time zone, day, hour
+// check time zone - https://secure.php.net/manual/en/timezones.php
+date_default_timezone_set('America/Sao_Paulo');
+$year = date('Y', time());
+$month = date('m', time());
+$monthword = date('M', time());
+$day = date('d', time());
+$hour = date('H', time());
+$minute = date('i', time());
+// time 1-14
+$time = '1';
+// time format - days or hours
+$timetype = 'days';
+$update = '(update frequency)';
+// check url
+function url_exists($url) {
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return ($code == 200);
+}
+
+// remove blank lines
+function rBlankLines($str) {  
+           $str = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $str);  
+  	   return $str;
+}
+
+function filelist($username, $repository, $branch, $file, $source){
+	$f = "! *** $repository:$file ***";
+	$link = "https://raw.githubusercontent.com/$username/$repository/$branch/$file";
+if (($source == 'local') && (file_exists("./$file"))) {
+    $filter = file_get_contents("./$file");
+   $result = "$f\n$filter";
+} elseif (($source == 'remote') && (url_exists($link))) {
+    $filter = file_get_contents($link);
+   $result = "$f\n$filter";
+} else {
+   $result = "! $file was not found";
+}
+return $result;
+}
+
+// Checksum
+function md5_base64($data) {
+  return preg_replace('/=+$/','',base64_encode(pack('H*',md5($data))));
+
+$ft[0] = "! $list1";
+$ft[1] = filelist($username, $repository, $branch, $file1, $source);
+$ft[2] = "! $list2";
+$ft[3] = filelist($username, $repository, $branch, $file2, $source);
+$ft[4] = "! $list3";
+$ft[5] = filelist($username, $repository, $branch, $file3, $source);
+$ft[6] = "! $list4";
+$ft[7] = filelist($username, $repository, $branch, $file4, $source);
+$ft[8] = "! $list5";
+$ft[9] = filelist($username, $repository, $branch, $file5, $source);
+$ft[10] = "! $list6";
+$ft[11] = filelist($username, $repository, $branch, $file6, $source);
+$ft[12] = "! $list7";
+$ft[13] = filelist($username, $repository, $branch, $file7, $source);
+$ft[14] = "! $list8";
+$ft[15] = filelist($username, $repository, $branch, $file8, $source);
+$ft[16] = "! $list9";
+$ft[17] = filelist($username, $repository, $branch, $file9, $source);
+$ft[18] = "! $list10";
+$ft[19] = filelist($username, $repository, $branch, $file10, $source);
+$ft[20] = "! $list11";
+$ft[21] = filelist($username, $repository, $branch, $file11, $source);
+$ft[22] = "! $list12";
+$ft[23] = filelist($username, $repository, $branch, $file12, $source);
+$ft[24] = "! $list13";
+$ft[25] = filelist($username, $repository, $branch, $file13, $source);
+$ft[26] = "! $list14";
+$ft[27] = filelist($username, $repository, $branch, $file14, $source);
+$ft[28] = "! $list15";
+$ft[29] = filelist($username, $repository, $branch, $file15, $source);
+$ft[30] = "! $list16";
+$ft[31] = filelist($username, $repository, $branch, $file16, $source);
+$ft[32] = "! $list17";
+$ft[33] = filelist($username, $repository, $branch, $file17, $source);
+$ft[34] = "! $list18";
+$ft[35] = filelist($username, $repository, $branch, $file18, $source);
+$ft[36] = "! $list19";
+$ft[37] = filelist($username, $repository, $branch, $file19, $source);
+$ft[38] = "! $list20";
+$ft[39] = filelist($username, $repository, $branch, $file20, $source);
+$ft[40] = "! $list21";
+$ft[41] = filelist($username, $repository, $branch, $file21, $source);
+$ft[42] = "! $list22";
+$ft[43] = filelist($username, $repository, $branch, $file22, $source);
+$ft[44] = "! $list23";
+$ft[45] = filelist($username, $repository, $branch, $file23, $source);
+$ft[46] = "! $list24";
+$ft[47] = filelist($username, $repository, $branch, $file24, $source);
+$ft[48] = "! $list25";
+$ft[49] = filelist($username, $repository, $branch, $file25, $source);
+$ft[50] = "! $list26";
+$ft[51] = filelist($username, $repository, $branch, $file26, $source);
+$ft[52] = "! $list27";
+$ft[53] = filelist($username, $repository, $branch, $file27, $source);
+$ft[54] = "! $list28";
+$ft[55] = filelist($username, $repository, $branch, $file28, $source);
+
+$abp = rBlankLines(implode("\n", $ft));
+$lines_arr = preg_split('/\n|\r/',$abp);
+$lines = count($lines_arr);
+
+$abpversion = "[Adblock Plus 2.0]\n";
+$info[0] = "! Version: $year$month$day$hour$minute";
+$info[1] = "! Title: $filtername";
+$info[2] = "! Last modified: $day $monthword $year $hour:$minute UTC";
+$info[3] = "! Expires: $time $timetype $update";
+$info[4] = "! Homepage: $home";
+$info[5] = "!\n";
+
+$abpinfo = implode("\n", $info);
+$recompile = $abpversion.$abpinfo.$abp;
+$chk = md5_base64($recompile);
+$check = "! Checksum: $chk\n";
+$validate = $abpversion.$check.$abpinfo.$abp;
+
+if (($export == '1') && ($lines != count($ft))) {
+    $textfile = trim($validate);
+    $myfile = fopen("$exportname", "w") or die("Unable to open file!");
+    fwrite($myfile, $textfile);
+    fclose($myfile);
+} elseif (($export == '0') && ($lines != count($ft))) {
+    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    header("Pragma: no-cache"); // HTTP 1.0.
+    header("Expires: 0"); // Proxies.
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Connection: close");
+    header('Content-Type: text/plain; charset=utf-8');
+    echo trim($validate);
+} else {
+    echo 'error';
+}
+
+?>
